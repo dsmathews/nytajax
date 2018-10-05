1ebe7452100da07e6289708fb679c48c4aabaf33
Ann <anntuck@anntuck.com>
2018-10-05 12:59:39 -0400

    needs nytimes api queries

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const getInput = function(event) {
 
     event.preventDefault();
-//Retreive User Input
+
+    //Retreive User Input
     console.log("search clicked");
     const keywordInput = $('#keyword').val();
     console.log(keywordInput);
@@ -12,8 +13,34 @@ const getInput = function(event) {
     const endYearInput = $('#endYear').val();
     console.log(endYearInput);
 
+    //Display Articles on the screen
+    displayResults(keywordInput, numRecordInput, startYearInput, endYearInput);
 }
 
+const displayResults = function(keywordInput, numRecordInput, startYearInput, endYearInput) {
+    console.log ("displayResults keywordInput", keywordInput);
+
+    const queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bccea3c2aaa1417790728926205111bc&q=shadow`;
+    // const queryURL = `https://api.iextrading.com/1.0/stock/${/quote/batch?types=quote,news&range=1m&last=1`;
+  
+
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+
+}).then(function(response) {
+
+    const article = response.name;
+    console.log(response);
+    // console.log(jason.stringify(response));
+
+    const artElement = $('<p>').text(`${article}`);
+
+    $('#results').append(artElement);
+
+});
+
+}
 
 
 
